804e8b3e6aad07978645e4864d0206f373bcbc03
Jonathan Azoff <jon@azof.fr>
2021-06-16 16:40:12 -0700

    change threshold for intersection

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -10,7 +10,7 @@
 	$: headerContent = 'is building a future beyond sustainability.'
 
 	onMount(() => {
-		const options = { root: null, rootMargin: "0px", threshold: [0.4] }
+		const options = { root: null, rootMargin: "0px", threshold: [0.2] }
 		Array.from(document.querySelectorAll("[data-header-content]")).forEach(s => {
 			const observer = new IntersectionObserver(([{intersectionRatio, target}]) => {
 				if (intersectionRatio >= options.threshold[0])
